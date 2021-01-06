9e81b687bc76a0bc0e2dad48119e0505ad6c34ec
happylay <1803455421@qq.com>
2021-01-07 06:11:21 +0800

    💖 添加sqlite3交叉编译命令

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+# 安装交叉编译工具
+# brew install mingw-w64
+# brew install FiloSottile/musl-cross/musl-cross
+
 # 编译linux
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gfctl-1.15.0-linux-amd64
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 CC=x86_64-linux-musl-gcc CGO_LDFLAGS="-static" go build -a -v -o gfctl-1.15.0.1-linux-amd64
 
 # 编译windows
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o gfctl-1.15.0-windows-amd64.exe
+CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc go build -v -o gfctl-1.15.0.1-windows-amd64.exe
 
 # 编译mac
-CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -o gfctl-1.15.0-darwin-amd64
+CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -o gfctl-1.15.0.1-darwin-amd64

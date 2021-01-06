567f6e7269729e81b34010f322a296b76df9f43d
happylay <1803455421@qq.com>
2021-01-07 05:01:52 +0800

    💖 更新脚本

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gfctl-1.15.0-linux-amd64
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o gfctl-1.15.0-windows-amd64.exe
 
 # 编译mac
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o gfctl-1.15.0-darwin-amd64
+CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -o gfctl-1.15.0-darwin-amd64
